71e2e4a27d920cfcb29d900df7d5bf47267c4066
Dennison Richter <drichter@amoebaman.net>
2014-03-10 01:18:21 -0400

    Add configuration file loading utility

--- src/net/amoebaman/utils/GenUtil.java
+++ src/net/amoebaman/utils/GenUtil.java
@@ -1,5 +1,6 @@
 package net.amoebaman.utils;
 
+import java.io.*;
 import java.util.Collection;
 import java.util.Iterator;
 
@@ -8,8 +9,48 @@ import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Projectile;
 import org.bukkit.entity.Tameable;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.plugin.Plugin;
 
 public class GenUtil {
+	
+	/**
+	 * Gets a YAML (.yml) configuration file from the plugin's folder.  If the file does not
+	 * exist and a default is embedded in the plugin's jarfile, the default will be copied
+	 * to the plugin's directory and loaded instead.
+	 * 
+	 * @param plugin a plugin
+	 * @param name the name of a configuration
+	 * @return the configuration file
+	 */
+	public static File getConfigFile(Plugin plugin, String name) {
+		name.replace(".yml", "");
+		try {
+			File file = new File(plugin.getDataFolder().getPath() + File.separator + name + ".yml");
+			file.getParentFile().mkdirs();
+			if (!file.exists()) {
+				plugin.getLogger().info("plugins/KitMaster/" + name + ".yml was not found");
+				plugin.getLogger().info("Writing new file with default contents");
+				file.createNewFile();
+				file.setWritable(true);
+				InputStream preset = plugin.getClass().getResourceAsStream("/defaults/" + name + ".yml");
+				if (preset != null) {
+					BufferedReader reader = new BufferedReader(new InputStreamReader(preset));
+					BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+					while (reader.ready()) {
+						writer.write(reader.readLine());
+						writer.newLine();
+					}
+					reader.close();
+					writer.close();
+				}
+			}
+			return file;
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
     /**
      * Gets the {@link LivingEntity} truly responsible for the damage, if there is one.
