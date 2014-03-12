3518650a9fced940babdbf3da94ce1b404b90909
Dennison Richter <drichter@amoebaman.net>
2014-03-12 01:11:24 -0400

    Make utility method static

--- src/net/amoebaman/utils/nms/AttributeStorage.java
+++ src/net/amoebaman/utils/nms/AttributeStorage.java
@@ -14,45 +14,34 @@ import net.amoebaman.utils.nms.Attributes.Attribute;
  */
 public class AttributeStorage {
 	
-	private ItemStack stack;
-	private final UUID uuid;
-	
-	private AttributeStorage(ItemStack stack, UUID uuid) {
-		if(stack == null || uuid == null)
-			throw new IllegalStateException("item and uuid cannot be null");
-		this.stack = stack;
-		this.uuid = uuid;
+	public static UUID addData(ItemStack item, String data){
+		UUID id = UUID.randomUUID();
+		setData(item, id, data);
+		return id;
 	}
 	
-	public static AttributeStorage getStorage(ItemStack stack, UUID uuid) {
-		return new AttributeStorage(stack, uuid);
-	}
-	
-	public String getData() {
-		Attribute current = getAttribute(new Attributes(stack), uuid);
-		return current != null ? current.name : null;
-	}
-	
-	public void setData(String data) {
-		Attributes attributes = new Attributes(stack);
-		Attribute current = getAttribute(attributes, uuid);
+	public static void setData(ItemStack item, UUID uuid, String data){
+		Attributes attrbs = new Attributes(item);
+		Attribute attrb = getAttribute(attrbs, uuid);
 		
-		if(current == null) {
-			Attribute attrb = new Attribute();
+		if(attrb == null){
+			attrb = new Attribute();
 			attrb.uuid = uuid;
 			attrb.name = data;
-			attributes.add(attrb);
+			attrbs.add(attrb);
 		}
-		else
-			current.name = data;
-		this.stack = attributes.getStack();
+		else{
+			attrb.name = data;
+			attrbs.update(attrb);
+		}
 	}
 	
-	public ItemStack getTarget() {
-		return stack;
+	public static String getData(ItemStack item, UUID uuid){
+		Attribute attrb = getAttribute(new Attributes(item), uuid);
+		return attrb != null ? attrb.name : null;
 	}
 	
-	private Attribute getAttribute(Attributes attrbs, UUID id) {
+	private static Attribute getAttribute(Attributes attrbs, UUID id) {
 		for(Attribute attrb : attrbs.getAttributes())
 			if(attrb.uuid.equals(id))
 				return attrb;
