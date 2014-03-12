73cdbee19d4fd9e398655882d77d33017911e6a0
Dennison Richter <drichter@amoebaman.net>
2014-03-12 00:50:49 -0400

    Javadoc

--- src/net/amoebaman/utils/nms/ParticleEffect.java
+++ src/net/amoebaman/utils/nms/ParticleEffect.java
@@ -11,40 +11,111 @@ import org.bukkit.entity.Player;
  */
 public enum ParticleEffect {
 	
+	//TODO -> basic javadoc for each of these effects
+	
+	/** Large explosion particles */
 	HUGE_EXPLOSION("hugeexplosion"),
+	
+	/** Small explosion particles */
 	LARGE_EXPLODE("largeexplode"),
+	
+	/** Particles given off as a firework ascends */
 	FIREWORKS_SPARK("fireworksSpark"),
+	
+	/** Bubbles caused by entities interacting with water */
 	BUBBLE("bubble"),
+	
+	/** Point particles suspended underwater */
 	SUSPEND("suspended"),
+	
+	/** Point particles found near bedrock */
 	DEPTH_SUSPEND("depthsuspend"),
+	
+	/** Point particles given off by mycelium */
 	TOWN_AURA("townaura"),
+	
+	/** Critical hit particles */
 	CRIT("crit"),
+	
+	/** Enchanted weapon hit particles */
 	MAGIC_CRIT("magicCrit"),
+	
+	/** Small smoke particles from torches */
 	SMOKE("smoke"),
+	
+	/** Opaque potion effect swirls */
 	MOB_SPELL("mobSpell"),
+	
+	/** Nearly transparent potion effect swirls */
 	MOB_SPELL_AMBIENT("mobSpellAmbient"),
+	
+	/** Potion effect swirls from breaking splash potions */
 	SPELL("spell"),
+	
+	/** Particles from instant healt/damage potions */
 	INSTANT_SPELL("instantSpell"),
+	
+	/** That weird crap over witches' heads */
 	WITCH_MAGIC("witchMagic"),
+	
+	/** Notes from note blocks */
 	NOTE("note"),
+	
+	/** Particles given off by endermen and nether portals */
 	PORTAL("portal"),
+	
+	/** Runes sucked in by enchanting tables from bookshelves */
 	ENCHANTMENT_TABLE("enchantmenttable"),
+	
+	/** The old explosion particles */
 	EXPLODE("explode"),
+	
+	/** Flames from funances, torches, and mobspawners */
 	FLAME("flame"),
+	
+	/** Embers with smoke trails ejected from lava */
 	LAVA("lava"),
+	
+	/** Weird square things that are supposed to be footsteps */
 	FOOTSTEP("footstep"),
+	
+	/** Water particles from rain hitting the ground */
 	SPLASH("splash"),
+	
+	/** Big smoke particles given off by fires */
 	LARGE_SMOKE("largesmoke"),
+	
+	/** Poofs given off by dying mobs */
 	CLOUD("cloud"),
+	
+	/** Red puffs given off by activated restone */
 	RED_DUST("reddust"),
+	
+	/** Particles from when thrown snowballs break */
 	SNOWBALL_POOF("snowballpoof"),
+	
+	/** Water dripping through a block */
 	DRIP_WATER("dripWater"),
+	
+	/** Lava dripping through a block */
 	DRIP_LAVA("dripLava"),
+	
+	/** Dunno... */
 	SNOW_SHOVEL("snowshovel"),
+	
+	/** Bits of slime from when slimes jump */
 	SLIME("slime"),
+	
+	/** Hearts given off from breeding animals */
 	HEART("heart"),
+	
+	/** Particles indicating villager happiness */
 	ANGRY_VILLAGER("angryVillager"),
+	
+	/** Particles indidcating villager irritation */
 	HAPPY_VILLAGER("happyVillager"),
+	
+	/** Shows players in creative mode where barriers are */
 	BARRIER("barrier")
 	;
 	
