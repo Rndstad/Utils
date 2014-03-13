9825447b9eb7cb1ca8bb437918297910fc68788f
Dennison Richter <drichter@amoebaman.net>
2014-03-13 05:34:05 -0400

    Fix constructing formats from strings

--- src/net/amoebaman/utils/chat/Format.java
+++ src/net/amoebaman/utils/chat/Format.java
@@ -5,8 +5,6 @@ import java.util.Set;
 
 import org.bukkit.ChatColor;
 
-import net.minecraft.util.com.google.common.collect.Lists;
-
 /**
  * Consolidated class for storing a compound chat color/style format consisting of a
  * single color (can't display multiple colors at once) and multiple styles.
@@ -28,22 +26,32 @@ public class Format{
 	 * @param codes some color codes
 	 */
 	public Format(ChatColor... codes){
+		setup(codes);
+	}
+	
+	public Format(String sequence){
+		String[] codes = ChatColor.getLastColors(ChatColor.translateAlternateColorCodes('&', sequence)).split(String.valueOf(ChatColor.COLOR_CHAR));
+		ChatColor[] colors = new ChatColor[codes.length];
+		for(int i = 0; i < codes.length; i++)
+			if(!codes[i].isEmpty())
+				colors[i] = ChatColor.getByChar(codes[i]);
+		setup(colors);
+	}
+	
+	private void setup(ChatColor... codes){
 		styles = new HashSet<ChatColor>();
 		for(ChatColor code : codes)
-			if(code != null && code.isColor())
-				color = code;
-			else
-				styles.add(code);
+			if(code != null)
+				if(code.isColor())
+					color = code;
+				else
+					styles.add(code);
 		
 		string = "";
 		if(color != null)
 			string += color;
 		for(ChatColor style : styles)
 			string += style;
-	}
-	
-	public Format(String sequence){
-		this(Lists.newArrayList(ChatColor.getLastColors(ChatColor.translateAlternateColorCodes('&', sequence)).split(String.valueOf(ChatColor.COLOR_CHAR))).toArray(new ChatColor[0]));
 	}
 	
 	public ChatColor color(){ return color; }
