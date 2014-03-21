cc2480d8d085c5c98a3175d39611d70326bd0bd1
Dennison Richter <drichter@amoebaman.net>
2014-03-20 21:52:39 -0400

    Add YAML reader and writer

---
+++ src/net/amoebaman/utils/YamlReader.java
@@ -0,0 +1,149 @@
+package net.amoebaman.utils;
+
+import java.util.*;
+
+import org.bukkit.*;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.*;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import net.amoebaman.utils.nms.Attributes;
+import net.amoebaman.utils.nms.Attributes.Attribute;
+import net.amoebaman.utils.nms.Attributes.AttributeType;
+import net.amoebaman.utils.nms.Attributes.Operation;
+
+
+public class YamlReader{
+
+public ItemStack readItem(ConfigurationSection section) {
+		
+		ItemStack item = new ItemStack(Material.AIR);
+		try {
+			
+			item.setType(Material.getMaterial(section.getString("type")));
+			item.setDurability((short) section.getInt("data"));
+			item.setAmount(section.getInt("amount"));
+			
+			if(section.isConfigurationSection("enchants")){
+				ItemMeta meta = item.getItemMeta();
+				for(String key : section.getConfigurationSection("enchants").getKeys(false))
+					if (meta instanceof EnchantmentStorageMeta)
+						((EnchantmentStorageMeta) meta).addEnchant(Enchantment.getByName(key), section.getInt("enchants." + key), true);
+					else
+						meta.addEnchant(Enchantment.getByName(key), section.getInt("enchants." + key), true);
+				item.setItemMeta(meta);
+			}
+			
+			if(section.isConfigurationSection("meta")){
+				ItemMeta meta = item.getItemMeta();
+				if(section.isString("meta.name"))
+					meta.setDisplayName(section.getString("meta.name"));
+				if(section.isList("meta.lore"))
+					meta.setLore(section.getStringList("meta.lore"));
+				if(section.isInt("meta.color"))
+					((LeatherArmorMeta) meta).setColor(Color.fromRGB(section.getInt("meta.color")));
+				if(section.isString("meta.skull"))
+					((SkullMeta) meta).setOwner(section.getString("meta.skull"));
+				if(section.isBoolean("meta.map"))
+					((MapMeta) meta).setScaling(section.getBoolean("meta.map"));
+				if(section.isConfigurationSection("meta.effects"))
+					for(String key : section.getConfigurationSection("meta.effects").getKeys(false))
+						((PotionMeta) meta).addCustomEffect(readEffect(section.getConfigurationSection("meta.effects." + key)), true);
+				if(section.isConfigurationSection("meta.book"))
+					readBook((BookMeta) meta, section.getConfigurationSection("meta.book"));
+				if(section.isConfigurationSection("meta.burst"))
+					((FireworkEffectMeta) meta).setEffect(readBurst(section.getConfigurationSection("meta.burst")));
+				if(section.isConfigurationSection("meta.firework"))
+					readFirework((FireworkMeta) meta, section.getConfigurationSection("meta.firework"));
+			}
+			
+			if(section.isConfigurationSection("attributes")){
+				Attributes attrbs = new Attributes(item);
+				for(String key : section.getConfigurationSection("attributes").getKeys(false))
+					attrbs.add(readAttribute(section.getConfigurationSection("attributes." + key)));
+				item = attrbs.getStack();
+			}
+			
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		return item;
+	}
+	
+	public PotionEffect readEffect(ConfigurationSection section) {
+		PotionEffectType type = PotionEffectType.getByName(section.getString("type"));
+		int duration = section.getInt("duration");
+		int amplifier = section.getInt("amplifier");
+		return new PotionEffect(type, duration, amplifier);
+	}
+	
+	public BookMeta readBook(BookMeta book, ConfigurationSection section) {
+		book.setTitle(section.getString("title"));
+		book.setAuthor(section.getString("author"));
+		book.setPages(section.getStringList("pages"));
+		return book;
+	}
+	
+	public BookMeta readBook(ConfigurationSection section) {
+		return readBook((BookMeta) Bukkit.getItemFactory().getItemMeta(Material.WRITTEN_BOOK), section);
+	}
+	
+	public FireworkEffect readBurst(ConfigurationSection section) {
+		FireworkEffect.Builder burst = FireworkEffect.builder();
+		burst.with(FireworkEffect.Type.valueOf(section.getString("type")));
+		List<Color> primary = new ArrayList<Color>();
+		for(String key : section.getConfigurationSection("primary").getKeys(false))
+			primary.add(Color.fromRGB(section.getInt("primary." + key)));
+		burst.withColor(primary);
+		List<Color> fade = new ArrayList<Color>();
+		for(String key : section.getConfigurationSection("fade").getKeys(false))
+			fade.add(Color.fromRGB(section.getInt("fade." + key)));
+		burst.withFade(fade);
+		if(section.getBoolean("flicker"))
+			burst.withFlicker();
+		if(section.getBoolean("trail"))
+			burst.withTrail();
+		try{
+			return burst.build();
+		}
+		catch(Exception e){
+			return null;
+		}
+	}
+	
+	public FireworkMeta readFirework(FireworkMeta firework, ConfigurationSection section) {
+		firework.setPower(section.getInt("fuse"));
+		for(String key : section.getConfigurationSection("bursts").getKeys(false))
+			firework.addEffect(readBurst(section.getConfigurationSection("bursts." + key)));
+		return firework;
+	}
+	
+	public FireworkMeta readFirework(ConfigurationSection section) {
+		return readFirework((FireworkMeta) Bukkit.getItemFactory().getItemMeta(Material.FIREWORK), section);
+	}
+	
+	public Location readLoc(ConfigurationSection section){
+		Location loc = Bukkit.getWorlds().get(0).getSpawnLocation();
+		loc.setX(section.getDouble("x"));
+		loc.setY(section.getDouble("y"));
+		loc.setZ(section.getDouble("z"));
+		loc.setPitch((float) section.getDouble("pitch", 0.0));
+		loc.setYaw((float) section.getDouble("yaw", 0.0));
+		return loc;
+	}
+	
+	public Attribute readAttribute(ConfigurationSection section){
+		Attribute attrb = new Attribute();
+		attrb.uuid = UUID.fromString(section.getString("uuid"));
+		attrb.name = section.getString("name");
+		attrb.type = AttributeType.fromId(section.getString("attrb"));
+		attrb.op = Operation.valueOf(section.getString("op"));
+		attrb.value = section.getDouble("value");
+		return attrb;
+	}
+	
+}
---
+++ src/net/amoebaman/utils/YamlWriter.java
@@ -0,0 +1,128 @@
+package net.amoebaman.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.*;
+import org.bukkit.potion.PotionEffect;
+
+import net.amoebaman.utils.nms.Attributes;
+import net.amoebaman.utils.nms.Attributes.Attribute;
+
+public class YamlWriter{
+	
+	public static void writeItem(ItemStack item, ConfigurationSection section) {
+		
+		if (item != null && item.getType() != Material.AIR) {
+			
+			section.set("type", item.getType().name());
+			section.set("data", item.getDurability());
+			section.set("amount", item.getAmount());
+			
+			section.createSection("enchants");
+			for (Enchantment enc : item.getEnchantments().keySet())
+				section.set("enchants." + enc.getName(), item.getEnchantmentLevel(enc));
+			if (item.hasItemMeta() && item.getItemMeta() instanceof EnchantmentStorageMeta) {
+				EnchantmentStorageMeta meta = (EnchantmentStorageMeta) item.getItemMeta();
+				for (Enchantment enc : meta.getEnchants().keySet())
+					section.set("enchants." + enc.getName(), meta.getEnchantLevel(enc));
+			}
+			
+			if (item.hasItemMeta()) {
+				ItemMeta meta = item.getItemMeta();
+				section.createSection("meta");
+				
+				if (meta.hasDisplayName())
+					section.set("meta.name", meta.getDisplayName());
+				if (meta.hasLore())
+					section.set("meta.lore", meta.getLore());
+				if (meta instanceof LeatherArmorMeta)
+					section.set("meta.color", ((LeatherArmorMeta) meta).getColor().asRGB());
+				if (meta instanceof SkullMeta)
+					section.set("skull", ((SkullMeta) meta).getOwner());
+				if (meta instanceof MapMeta)
+					section.set("map", ((MapMeta) meta).isScaling());
+				if (meta instanceof PotionMeta && ((PotionMeta) meta).hasCustomEffects()){
+					section.createSection("effects");
+					for(int i = 0; i < ((PotionMeta) meta).getCustomEffects().size(); i++)
+						writeEffect(((PotionMeta) meta).getCustomEffects().get(i), section.createSection("effects." + i));
+				}
+				if (meta instanceof BookMeta)
+					writeBook((BookMeta) meta, section.createSection("book"));
+				if (meta instanceof FireworkEffectMeta)
+					writeBurst(((FireworkEffectMeta) meta).getEffect(), section.createSection("burst"));
+				if (meta instanceof FireworkMeta)
+					writeFirework((FireworkMeta) meta, section.createSection("firework"));
+			}
+			
+			List<Attribute> attrbs = new Attributes(item).getAttributes();
+			if(attrbs.size() > 0){
+				section.createSection("attributes");
+				for(int i = 0; i < attrbs.size(); i++)
+					writeAttribute(attrbs.get(i), section.createSection("attributes." + i));
+			}
+			
+		}
+		
+	}
+	
+	public static void writeEffect(PotionEffect effect, ConfigurationSection section) {
+		section.set("type", effect.getType().getName());
+		section.set("duration", effect.getDuration());
+		section.set("amplifier", effect.getAmplifier());
+	}
+	
+	public static void writeBook(BookMeta book, ConfigurationSection section){
+		section.set("title", book.getTitle());
+		section.set("author", book.getAuthor());
+		section.set("pages", book.getPages());
+	}
+	
+	public static void writeBurst(FireworkEffect burst, ConfigurationSection section){
+		section.set("type", burst.getType().name());
+		List<Integer> primary = new ArrayList<Integer>();
+		for (Color color : burst.getColors())
+			primary.add(color.asRGB());
+		section.set("primary", primary);
+		List<Integer> fade = new ArrayList<Integer>();
+		for (Color color : burst.getFadeColors())
+			fade.add(color.asRGB());
+		section.set("fade", fade);
+		section.set("flicker", burst.hasFlicker());
+		section.set("trail", burst.hasTrail());
+	}
+	
+	public static void writeFirework(FireworkMeta firework, ConfigurationSection section) {
+		section.set("fuse", firework.getPower());
+		section.createSection("bursts");
+		for(int i = 0; i < firework.getEffectsSize(); i++)
+			writeBurst(firework.getEffects().get(i), section.createSection("bursts." + i));
+	}
+	
+	public static void writeLoc(Location loc, boolean round, boolean rotation, ConfigurationSection section){
+		section.set("world", loc.getWorld().getName());
+		section.set("x", round ? loc.getBlockX() + 0.5 : loc.getX());
+		section.set("y", round ? loc.getBlockY() : loc.getY());
+		section.set("z", round ? loc.getBlockZ() + 0.5 : loc.getZ());
+		if (rotation) {
+			section.set("pitch", round ? Math.round(loc.getPitch() * 22.5) / 22.5 : loc.getPitch());
+			section.set("yaw", round ? Math.round(loc.getYaw() * 22.5) / 22.5 : loc.getYaw());
+		}
+	}
+	
+	public static void writeAttribute(Attribute attrb, ConfigurationSection section){
+		section.set("uuid", attrb.uuid.toString());
+		section.set("name", attrb.name);
+		section.set("attrb", attrb.type.identifier);
+		section.set("op", attrb.op.name());
+		section.set("value", attrb.value);
+	}
+	
+}
