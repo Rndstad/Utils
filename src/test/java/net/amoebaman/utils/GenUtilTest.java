9605d081781181dde876a4409527562a2aaf719f
Dennison Richter <drichter@amoebaman.net>
2014-04-14 01:39:13 -0400

    JUnit! :D

---
+++ src/test/java/net/amoebaman/utils/GenUtilTest.java
@@ -0,0 +1,76 @@
+package net.amoebaman.utils;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.List;
+import java.util.Set;
+
+import org.bukkit.entity.Player;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import com.google.common.collect.Sets;
+
+import net.minecraft.util.com.google.common.collect.Lists;
+
+@RunWith(PowerMockRunner.class)
+public class GenUtilTest{
+	
+	public static Player mockPlayer(String name){
+		Player player = Mockito.mock(Player.class);
+		Mockito.when(player.getName()).thenReturn(name);
+		return player;
+	}
+	
+	@Ignore
+	public void testGetConfigFile(){
+		fail("Can't be implemented");
+	}
+	
+	@Test
+	public void testGetRandomElement(){
+		Set set = Sets.newHashSet("I", "like", "cheese");
+		assertTrue("set actually contains the random element selected", set.contains(GenUtil.getRandomElement(set)));
+	}
+	
+	@Test
+	public void testObjectsToStrings(){
+		List<Object> objects = Lists.newArrayList(
+			"potato",
+			" = ",
+			new Object(){ public String toString(){ return "spud"; } }
+		);
+		
+		assertEquals("potato = spud", GenUtil.concat(objects));
+	}
+	
+	@Test
+	public void testPlayersToNames(){
+		List<Player> players = Lists.newArrayList(mockPlayer("AmoebaMan"), mockPlayer("Kainzo"), mockPlayer("Dinnerbone"));
+		List<String> names = GenUtil.playersToNames(players);
+		
+		assertEquals("AmoebaMan, Kainzo, Dinnerbone", GenUtil.concat(names, ", "));
+	}
+	
+	@Test
+	public void testConcat(){
+		List<Object> list = Lists.newArrayList(mockPlayer("Pie"), "is", "mighty", "delicious!");
+		String concat = GenUtil.concat(list, "Bob said, \"", " ", "\"");
+		
+		assertEquals("Bob said, \"Pie is mighty delicious!\"", concat);
+	}
+	
+	@Test
+	public void testExpand(){
+		Object original = Lists.newArrayList("I ", "am ", new String[]{"a ", "huge "}, Lists.newArrayList("fan ", "of ", new String[]{"both ", "pie "}, "and "), "cheese");
+		List<Object> expanded = GenUtil.expand(original, "!");
+		
+		assertEquals("I am a huge fan of both pie and cheese!", GenUtil.concat(expanded));
+	}
+	
+}
---
+++ src/test/java/net/amoebaman/utils/nms/ReflectionUtilTest.java
@@ -0,0 +1,62 @@
+package net.amoebaman.utils.nms;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import javax.tools.JavaFileManager.Location;
+
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.material.MaterialData;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@RunWith(PowerMockRunner.class)
+public class ReflectionUtilTest{
+	
+	@Ignore
+	public void testGetVersion(){
+		fail("Can't be implemented");
+	}
+	
+	@Ignore
+	public void testGetNMSClass(){
+		fail("Can't be implemented");
+	}
+	
+	@Ignore
+	public void testGetOBCClass(){
+		fail("Can't be implemented");
+	}
+	
+	@Ignore
+	public void testGetHandle(){
+		fail("Can't be implemented");
+	}
+	
+	@Test
+	public void testGetField(){
+		Field field = ReflectionUtil.getField(MaterialData.class, "type");
+		assertNotNull(field);
+	}
+	
+	@Test
+	public void testGetMethod(){
+		Method method = ReflectionUtil.getMethod(Player.class, "getName");
+		assertNotNull(method);
+	}
+	
+	@Test
+	public void testClassListEqual(){
+		assertTrue(ReflectionUtil.classListEqual(new Class<?>[]{int.class, String.class, Player.class}, new Class<?>[]{int.class, String.class, Player.class}));
+		assertFalse(ReflectionUtil.classListEqual(new Class<?>[]{World.class, Location.class, Player.class}, new Class<?>[]{World.class, Player.class, Location.class}));
+	}
+	
+}
